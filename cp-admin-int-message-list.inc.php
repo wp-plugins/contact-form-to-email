aedb159ba8671f01cc69a985ae3b08fa6a3288d2
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-27 14:37:45 +0000

    Language domain updated
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-to-email/tags/1.1.7@1254498 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cp-admin-int-message-list.inc.php
+++ cp-admin-int-message-list.inc.php
@@ -137,8 +137,8 @@ echo paginate_links(  array(
     'end_size'     => 1,
     'mid_size'     => 2,
     'prev_next'    => True,
-    'prev_text'    => __('&laquo; Previous','cfte'),
-    'next_text'    => __('Next &raquo;','cfte'),
+    'prev_text'    => __('&laquo; Previous','contact-form-to-email'),
+    'next_text'    => __('Next &raquo;','contact-form-to-email'),
     'type'         => 'plain',
     'add_args'     => False
     ) );
